073ce443be916aa34bc862b003672b669248ece8
louaysalh <loaysalh5@gmail.com>
2025-07-05 18:47:08 +0300

    add simple bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,12 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_app/constants.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
+import 'package:notes_app/simple_bloc_observer.dart';
 import 'package:notes_app/views/notes_view.dart';
 
 void main() async {
   await Hive.initFlutter();
+  Bloc.observer = SimpleBlocObserver();
   await Hive.openBox(kNotesBox);
   Hive.registerAdapter(NoteModelAdapter());
   runApp(const NotesApp());
@@ -19,9 +21,7 @@ class NotesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (context)=> AddNoteCubit()),
-      ],
+      providers: [BlocProvider(create: (context) => AddNoteCubit())],
       child: MaterialApp(
         theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
         home: const NotesView(),
---
+++ lib/simple_bloc_observer.dart
@@ -0,0 +1,38 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/widgets.dart';
+
+class SimpleBlocObserver
+    implements
+        BlocObserver // implement enforce you implement every fuction in class
+        {
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    // TODO: implement onChange
+    debugPrint('change=$change'); 
+  }
+
+  @override
+  void onClose(BlocBase bloc) {
+    // TODO: implement onClose
+  }
+
+  @override
+  void onCreate(BlocBase bloc) {
+    // TODO: implement onCreate
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    // TODO: implement onError
+  }
+
+  @override
+  void onEvent(Bloc bloc, Object? event) {
+    // TODO: implement onEvent
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    // TODO: implement onTransition
+  }
+}
