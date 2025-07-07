f5869ac359016587582fdac0e9a4c873cc4504d5
louaysalh <loaysalh5@gmail.com>
2025-07-08 01:10:14 +0300

    display notes part one

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -8,15 +8,10 @@ part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
+  List<NoteModel>? notes;
   fetchAllNotes() async {
-    try {
-      var notesBox = Hive.box<NoteModel>(kNotesBox);
-      List<NoteModel> notes =
-          notesBox.values
-              .toList(); //values return iterable (collection of NoteModel )
-      emit(NotesSuccess(notes:notes));
-    } catch (e) {
-      emit(NotesFailure(errMessage: (e.toString())));
-    }
+    var notesBox = Hive.box<NoteModel>(kNotesBox);
+    notes =notesBox.values.toList(); //values return iterable (collection of NoteModel )
+    
   }
 }
--- lib/widgets/notes_list_view.dart
+++ lib/widgets/notes_list_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
+import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/widgets/custom_note_item.dart';
 
 class NotesListView extends StatelessWidget {
@@ -6,18 +9,24 @@ class NotesListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 16),
-      child: ListView.builder(
-        // expanded unbounded height
-        padding: EdgeInsets.zero,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(vertical: 4),
-            child: const NoteItem(),
-          );
-        },
-      ),
+    return BlocBuilder<NotesCubit,NotesState>(
+      builder: (context, state) {
+        List <NoteModel> notes=BlocProvider.of<NotesCubit>(context).notes??[] ;
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          child: ListView.builder(
+            // expanded unbounded height
+            itemCount: notes.length ,
+            padding: EdgeInsets.zero,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 4),
+                child: const NoteItem(),
+              );
+            },
+          ),
+        );
+      },
     );
   }
 }
